278359eb89112ec14f2374777f8c8b2d50dde0c1
2muchfun <tuphan2911@yahoo.de>
2022-11-10 15:22:51 +0100

    add test for sort algo

--- lab02/src/test/java/de/bht/pr2/lab02/part1/MyTest.java
+++ lab02/src/test/java/de/bht/pr2/lab02/part1/MyTest.java
@@ -4,6 +4,7 @@ package de.bht.pr2.lab02.part1;
 import de.bht.pr2.lab01.Student;
 
 
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,11 +12,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class MyTest {
 
   @Test
-  void testSomething() throws NotPaidTuitionFeeException, RegistrationNumberException,
-          WrongCourseOfStudiesException, StudentParseException {
+  void testSomething() throws Exception {
 
     Student student = new Student("Zoe Zoo,70026,Technische Informatik,312");
-    assertEquals("Zoe Zoo", student.getName());
+    assertEquals("Zoe Zoo", student.name);
     //assertEquals("Zoe Zoo",  student.);
   }
 }

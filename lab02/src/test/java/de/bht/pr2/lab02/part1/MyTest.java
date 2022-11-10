20dce62c3f4f36435515bb9259e16ad3832feeb8
2muchfun <tuphan2911@yahoo.de>
2022-11-10 14:39:45 +0100

    add test

--- lab02/src/test/java/de/bht/pr2/lab02/part1/MyTest.java
+++ lab02/src/test/java/de/bht/pr2/lab02/part1/MyTest.java
@@ -1,11 +1,18 @@
 package de.bht.pr2.lab02.part1;
 
+
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MyTest {
 
   @Test
-  void testSomething() {
+  void testSomething() throws NotPaidTuitionFeeException, RegistrationNumberException,
+          WrongCourseOfStudiesException, StudentParseException {
 
+    Student student = new Student("Zoe Zoo,70026,Technische Informatik,312");
+    assertEquals("Zoe Zoo", student.getName());
+    //assertEquals("Zoe Zoo",  student.);
   }
 }

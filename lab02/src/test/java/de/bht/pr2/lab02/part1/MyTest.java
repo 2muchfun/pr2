a1aa4d54fa8994b579b67eba1748fa1197004411
2muchfun <tuphan2911@yahoo.de>
2022-11-10 15:22:51 +0100

    add test for sort algo

--- lab02/src/test/java/de/bht/pr2/lab02/part1/MyTest.java
+++ lab02/src/test/java/de/bht/pr2/lab02/part1/MyTest.java
@@ -1,5 +1,7 @@
-package de.bht.pr2.lab02.part1;
+//package de.bht.pr2.lab02.part1;
 
+package de.bht.pr2.lab02.part1;
+import de.bht.pr2.lab01.Student;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,11 +10,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class MyTest {
 
   @Test
-  void testSomething() throws NotPaidTuitionFeeException, RegistrationNumberException,
-          WrongCourseOfStudiesException, StudentParseException {
+  void testSomething() throws Exception {
 
     Student student = new Student("Zoe Zoo,70026,Technische Informatik,312");
-    assertEquals("Zoe Zoo", student.getName());
+    assertEquals("Zoe Zoo", student.name);
     //assertEquals("Zoe Zoo",  student.);
   }
 }
---
+++ lab02/src/test/java/de/bht/pr2/lab02/part1/MyTest_part2.java
@@ -0,0 +1,38 @@
+package de.bht.pr2.lab02.part1;
+import org.junit.jupiter.api.Test;
+import de.bht.pr2.lab02.part2.BubbleSort;
+import de.bht.pr2.lab02.part2.InsertionSort;
+import de.bht.pr2.lab02.part2.QuickSort;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MyTest_part2 {
+
+
+    @Test
+    void test_BubbleSort_sort()  {
+        int[] myIntArray = {4, 2, 3};
+        BubbleSort init = new BubbleSort();
+        int[] expect = {2,3,4};
+        assertArrayEquals(expect,init.sort(myIntArray));
+
+    }
+    @Test
+    void test_InsertionSort()  {
+        int[] myIntArray = {4, 2, 3};
+        InsertionSort init = new InsertionSort();
+        int[] expect = {2,3,4};
+        assertArrayEquals(expect,init.sort(myIntArray));
+
+    }
+    @Test
+    void test_QuickSort()  {
+        int[] myIntArray = {4, 2, 3};
+        QuickSort init = new QuickSort();
+        int[] expect = {2,3,4};
+        assertArrayEquals(expect,init.sort(myIntArray));
+
+    }
+}
+
